d703880dbc6ce97ba3e93c2d7d7676eb915d0f03
taurn kale <tkumarkale@gmail.com>
2024-10-08 20:34:58 +0530

    design is complete of calculator

---
+++ javascript.js
@@ -0,0 +1,18 @@
+const ANSWER = () => {
+    // first we take number
+    const one = document.querySelector("#one");
+    const two = document.querySelector("#two");
+    const three = document.querySelector("#three");
+    const four = document.querySelector("#four");
+    const five = document.querySelector("#five");
+    const six = document.querySelector("#six");
+    const seven = document.querySelector("#seven");
+    const eight = document.querySelector("#eight");
+    const nine = document.querySelector("#nine");
+    const zero = document.querySelector("#zero");
+    const doublezero = document.querySelector("#doublezero");
+    // second take new
+
+
+    
+};
